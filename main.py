cfa221b40df445c62ce1c094bdf6bf5647161fa7
vtruong1 <vtruong@infinitalk.co.jp>
2020-02-14 20:05:33 +0900

    delete trash req

--- main.py
+++ main.py
@@ -25,12 +25,4 @@ def process_request():
 
 if __name__ == '__main__':
 
-
-    ON_HEROKU = os.environ.get('ON_HEROKU')
-
-    if ON_HEROKU:
-        # get the heroku port
-        port = int(os.environ.get('PORT', 17995))  # as per OP comments default is 17995
-    else:
-        port = 5000
-    app.run('0.0.0.0', port=port)
+    app.run('0.0.0.0')
